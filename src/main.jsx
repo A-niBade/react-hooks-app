7c42231ae258beb0bacb490f56f49181c77c5041
Anibal <joan.maldonadoa@gmail.com>
2022-07-27 16:29:55 -0400

    Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HooksApp } from "./HooksApp";
+import { FormWithCustomHook } from "./02-useEffect/FormWIthCustomHook";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
+// import { CounterCustomHook } from "./01-useState/CounterCustomHook";
+// import { CounterApp } from "./01-useState/CounterApp";
+// import { HooksApp } from "./HooksApp";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HooksApp />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <FormWithCustomHook />
+  /* </React.StrictMode> */
 );
