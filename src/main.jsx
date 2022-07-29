bb9d8993e4d23d42636031c362a0620d0922bdf2
Anibal <joan.maldonadoa@gmail.com>
2022-07-28 21:25:29 -0400

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FormWithCustomHook } from "./02-useEffect/FormWIthCustomHook";
+// import { CallbckHook } from "./06-memos/CallbckHook";
+import { Padre } from "./07-tarea-memo/Padre";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { MemoHook } from "./06-memos/MemoHook";
+// import { Memorize } from "./06-memos/Memorize";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { FormWithCustomHook } from "./02-useEffect/FormWIthCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterCustomHook } from "./01-useState/CounterCustomHook";
 // import { CounterApp } from "./01-useState/CounterApp";
@@ -10,6 +17,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <FormWithCustomHook />
+  <Padre />
   /* </React.StrictMode> */
 );
