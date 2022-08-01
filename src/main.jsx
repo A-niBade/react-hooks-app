055b17c9b936d23aaa5d08eb468153bbcf19c6f3
Anibal <joan.maldonadoa@gmail.com>
2022-08-01 18:03:25 -0400

    TODO App

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { CallbckHook } from "./06-memos/CallbckHook";
-import { Padre } from "./07-tarea-memo/Padre";
+// import { Padre } from "./07-tarea-memo/Padre";
+import { TodoApp } from "./08-reducer/TodoApp";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
 // import { MemoHook } from "./06-memos/MemoHook";
@@ -12,11 +13,12 @@ import { Padre } from "./07-tarea-memo/Padre";
 // import { CounterCustomHook } from "./01-useState/CounterCustomHook";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { HooksApp } from "./HooksApp";
+// import "./08-reducer/intro-reducer";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <Padre />
+  <TodoApp />
   /* </React.StrictMode> */
 );
