44f5de5fdf1d27653f4b2b57ad5bcfc831469a5f
Anibal <joan.maldonadoa@gmail.com>
2022-08-03 18:46:27 -0400

    Fixed typo

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -4,12 +4,11 @@ export const useCounter = (initialValue = 10) => {
   const [counter, setCounter] = useState(initialValue);
 
   const increment = (value = 1) => {
-    setCounter(counter + value);
+    setCounter((current) => current + value);
   };
 
-  const sustract = (value = -1) => {
-    if (counter === 0) return;
-    setCounter(counter - value);
+  const sustract = (value = 1) => {
+    setCounter((current) => current - value);
   };
 
   const reset = () => {
