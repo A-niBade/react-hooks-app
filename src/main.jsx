30b52f219f4118e27e5e3257ac457b756ef40b8e
Anibal <joan.maldonadoa@gmail.com>
2022-08-02 20:45:41 -0400

    Main changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 // import { CallbckHook } from "./06-memos/CallbckHook";
 // import { Padre } from "./07-tarea-memo/Padre";
-import { TodoApp } from "./08-reducer/TodoApp";
+// import { TodoApp } from "./08-reducer/TodoApp";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
 // import { MemoHook } from "./06-memos/MemoHook";
@@ -14,11 +16,14 @@ import { TodoApp } from "./08-reducer/TodoApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { HooksApp } from "./HooksApp";
 // import "./08-reducer/intro-reducer";
+import { MainApp } from "./09-useContext/MainApp";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <TodoApp />
-  /* </React.StrictMode> */
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+    <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 );
